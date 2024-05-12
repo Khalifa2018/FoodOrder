dbe9a7683a14a2d327c8e95313a25a509baf8d6e
Ahmed Khalifa <ahmedkhalifa201395@yahoo.com>
2024-05-12 09:10:54 +0300

    add button css class

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -13,7 +13,7 @@ export default function Products({ products, isLoading }) {
               <p className="meal-item-price">${product.price}</p>
               <p className="meal-item-description">{product.description}</p>
               <p className="meal-item-actions">
-                <button className="active">Add to Cart</button>
+                <button className="button">Add to Cart</button>
               </p>
             </li>
           ))}
