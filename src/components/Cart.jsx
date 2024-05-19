b183651c78355a1a851108b13dacf8bd3364ba5f
Ahmed Khalifa <ahmedkhalifa201395@yahoo.com>
2024-05-19 11:06:20 +0300

    Edit cart description

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -9,7 +9,9 @@ export default function Cart() {
       <ul>
         {items.map((item) => (
           <li key={item.id} className="cart-item">
-            <p>{item.name}</p>
+            <p>
+              {item.name} - {item.quantity} x ${item.price}
+            </p>
             <div className="cart-item-actions">
               <button onClick={() => updateCartItemQuantity(item.id, -1)}>
                 -
